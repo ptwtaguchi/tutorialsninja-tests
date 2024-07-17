abca5eec1ea2d3423b9b64ec2a0f93946d15cdc4
sazanami <non_tokyo@yahoo.co.jp>
2024-07-17 18:55:50 +0900

    GitHub Actionsで動作させるため、ヘッドレスモードをTrueに変更

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser():
     playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     yield browser
     browser.close()
     playwright.stop()
